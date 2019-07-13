71dbf54f340452b0d16720ae6f871ee12bfa90ab
Sam Chung <schung2@deloitte.com.au>
2019-07-13 14:16:42 +1000

    Add post and deletereply

--- slackController.js
+++ slackController.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const qs = require('querystring');
+const CONSTANTS = require('./constants');
 
 /**
  * Reply formatted to Slack format.
@@ -10,6 +12,18 @@ function reply(response_type, text, attachments){
     var message = {
         "response_type" : response_type,
         "text" : text
+    }
+    if (attachments){
+        message.attachments = attachments;
+    }
+    return message
+}
+
+function deleteReply(response_type, text, attachments){
+    var message = {
+        "response_type" : response_type,
+        "text" : text,
+        "delete_original": true
     }
     if (attachments){
         message.attachments = attachments;
@@ -36,20 +50,30 @@ function spotifyToSlackAttachment(track, trigger_id) {
         };
 }
 
-async function sendToSlack(message, response_url, response_type){
+async function send(message, response_url){
     try {
-        await axios.post(response_url, {
-            "response_type" : response_type,
-            "text" : message
-        });
+        await axios.post(response_url, message);
         console.log("Message sent");
     } catch (error) {
         console.log(error);
     }
 }
 
+
+async function sendDialog(params){
+    return axios.post(`https://slack.com/api/dialog.open`, qs.stringify(params));
+}
+
+async function post(params){
+    return axios.post(`https://slack.com/api/chat.postMessage`, qs.stringify(params));
+}
+
+
 module.exports = {
-    sendToSlack,
+    send,
     reply,
-    spotifyToSlackAttachment
+    deleteReply,
+    post,
+    spotifyToSlackAttachment,
+    sendDialog
 };
