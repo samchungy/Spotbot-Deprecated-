9f7f4899c008354b696a42cce3c957e807887694
Sam Chung <schung2@deloitte.com.au>
2019-08-10 19:06:59 +1000

    Add requires

--- spotify/player/playerController.js
+++ spotify/player/playerController.js
@@ -1,3 +1,6 @@
+const spotifyController = require('../../core/spotifyController');
+const logger = require('../../log/winston');
+
 async function play(req, res) {
     logger.info("Play triggered");
     await spotifyController.play(req.body.response_url);
