31d00aa338295c5a1745778367bac495ba3e1a81
Sam Chung <schung2@deloitte.com.au>
2019-08-20 17:01:57 +1000

    Fix typo

--- components/spotify/tracks/tracksService.js
+++ components/spotify/tracks/tracksService.js
@@ -50,7 +50,7 @@ async function findPop(query, trigger_id, response_url) {
         logger.info(`Find popular tracks for query "${query}" triggered.`);
         let search_results = await tracks_api.getSearchTracks(query);
         let search_tracks = _.get(search_results, 'body.tracks.items');
-        search_tracks = _reverse(_.sortBy(search_tracks, ['popularity']));
+        search_tracks = _.reverse(_.sortBy(search_tracks, ['popularity']));
         if (search_tracks.length == 0) {
             //No Tracks found
             await slack_controller.reply(`:slightly_frowning_face: No popular tracks found for the search term "${query}". Try another search?`, null, response_url);
